06a97a4c6bcd11f2f273cddf64ba07bd62ae9d8e
Raphael Carvalho <Rcfrdeveloper@gmail.com>
2026-03-18 15:56:01 -0400

    add 1 exercise from condicional structure

---
+++ 01-fundamentals/01-programming-logic/02-conditional-structure/01_even_or_odden.py
@@ -0,0 +1,8 @@
+# Ask the user for an integer and display whether it is even or odd.
+
+number = int(input("Enter an intenger: "))
+
+if number % 2 == 0:
+    print("The number is even")
+else:
+    print("The number is odd")
